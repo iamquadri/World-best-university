e1e3a0c70b27fe438605cb0f8e8d13a3f0df20dd
iamquadri <quadrihosein69@gmail.com>
2023-03-02 14:04:33 +0100

    world best university with Html and javaScript

---
+++ form handler.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$email_form = 'quadriope69@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                    "Subject: $subject.\n".
+                        "User Message: $message .\n";
+
+
+$to = 'quadrihosein69@gmail.com';
+
+$headers = "From: $email_form \r\n";
+
+$headers . ="Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html")
+
+
+
+?>
